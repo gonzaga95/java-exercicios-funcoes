86ead5f541df591152f168171819af17f13a25fe
gonzaga95 <gonzagas.dudu@gmail.com>
2024-07-18 20:06:36 -0400

    update FuncoesComScanner print format add

--- src/FuncoesComScanner.java
+++ src/FuncoesComScanner.java
@@ -4,11 +4,12 @@ public class FuncoesComScanner {
 
     static public void calculaMedia(double nota1, double nota2, double nota3) {
         double media = (nota1 + nota2 + nota3) / 3;
+        String mediaFormatada = String.format("%.2f", media);
         if (media < 6){
             System.out.println("O aluno foi reprovado");
         }
 
-        System.out.println("A média do aluno foi: " + media);
+        System.out.printf("A média do aluno foi: " + mediaFormatada);
     }
 
     public static void main(String[] args) {
